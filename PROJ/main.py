5f1a357e37da648748f59ecf20a366df8b96ff3c
fcallanam <francisco.callanam@gmail.com>
2023-05-12 18:02:54 +0100

    conaconaconacona

--- PROJ/main.py
+++ PROJ/main.py
@@ -24,8 +24,10 @@ def db_connection():
     return db
 
 
+#TODO birthday e gender são uma complicação de merda, acho que é preferivel tirar, visto que n sao relevantes
 
-# User Registration
+
+# ==@=== REGISTRATIONS ===@==
 @app.route('/user/', methods=['POST'])
 def user_registration():
     logger.info('POST /user')
@@ -34,8 +36,274 @@ def user_registration():
     connection = db_connection()
     cur = connection.cursor()
 
-    # TODO username ou id
-    if 'username' 
+    if 'username' not in payload:
+        response = {'status': StatusCodes['api_error'], 'results': 'username value not in payload'}
+        return flask.jsonify(response)
+
+    # TODO remove gender | criar artista usando admin ?
+    statement = 'INSERT INTO user (username, password, full_name, birthday, email) VALUES (%s, %s, %s, %s, %s)'
+    values = (payload['username'], payload['password'], payload['full_name'], payload['birthday'], payload['email'])
+
+    try:
+        cur.execute(statement, values)
+
+        # commit the transaction
+        conn.commit()
+        response = {'status': StatusCodes['success'], 'results': f'Inserted user {payload["username"]}'}
+
+    except (Exception, psycopg2.DatabaseError) as error:
+        logger.error(f'POST /user/ - error: {error}')
+        response = {'status': StatusCodes['internal_error'], 'errors': str(error)}
+
+        # an error occurred, rollback
+        conn.rollback()
+
+    finally:
+        if conn is not None:
+            conn.close()
+
+    return flask.jsonify(response)
+
+
+@app.route('/administrator/', methods=['POST'])
+def admin_registration():
+    logger.info('POST /administrator')
+    payload = flask.request.get_json()
+
+    connection = db_connection()
+    cur = connection.cursor()
+
+    if 'username' not in payload:
+        response = {'status': StatusCodes['api_error'], 'results': 'username value not in payload'}
+        return flask.jsonify(response)
+
+    statement = 'INSERT INTO administrator (username) VALUES (%s)'
+    values = (payload['username'])
+
+    try:
+        cur.execute(statement, values)
+
+        # commit the transaction
+        conn.commit()
+        response = {'status': StatusCodes['success'], 'results': f'Inserted user {payload["username"]}'}
+
+    except (Exception, psycopg2.DatabaseError) as error:
+        logger.error(f'POST /administrator/ - error: {error}')
+        response = {'status': StatusCodes['internal_error'], 'errors': str(error)}
+
+        # an error occurred, rollback
+        conn.rollback()
+
+    finally:
+        if conn is not None:
+            conn.close()
+
+    return flask.jsonify(response)
+
+
+@app.route('/artist/', methods=['POST'])
+@token_required
+def artist_registration():
+    logger.info('POST /artist')
+    payload = flask.request.get_json()
+
+    connection = db_connection()
+    cur = connection.cursor()
+
+    if 'username' not in payload:
+        response = {'status': StatusCodes['api_error'], 'results': 'username value not in payload'}
+        return flask.jsonify(response)
+
+    # TODO admin e userr_username podem ser desnecessários para criar || Also int para ismn estará correto?
+    statement = 'INSERT INTO artist (artistic_name, song_ismn, label_name, administrator_u, userr_username) VALUES (%s, %d, %s, %s, %s)'
+    values = (payload['artistic_name'], int(payload['song_ismn']), payload['label_name'], payload['administrator_u'], payload['userr_username'])
+
+    try:
+        cur.execute(statement, values)
+
+        # commit the transaction
+        conn.commit()
+        response = {'status': StatusCodes['success'], 'results': f'Inserted user {payload["username"]}'}
+
+    except (Exception, psycopg2.DatabaseError) as error:
+        logger.error(f'POST /artist/ - error: {error}')
+        response = {'status': StatusCodes['internal_error'], 'errors': str(error)}
+
+        # an error occurred, rollback
+        conn.rollback()
+
+    finally:
+        if conn is not None:
+            conn.close()
+
+    return flask.jsonify(response)
+
+
+# ==@=== AUTHENTICATIONS ===@==
+@app.route("/user/", methods = ['PUT'])
+def user_authentication():
+    payload = request.get_json()
+
+    conn = db_connection()
+    cur = conn.cursor()
+
+    logging_logger.logger.info("AUTENTICATE /user")
+    logging_logger.logger.debug(f'payload: {payload}')
+
+    cur.execute("SELECT password FROM user WHERE username=%s", (payload["username"],))
+    logging_logger.logger.debug(f'payload: {payload["username"]}')
+
+    password = cur.fetchall()
+    if (len(password) == 0):
+        result = {"Error:": "Invalid Login"}
+        return jsonify(result)
+
+    if (password[0][0] != payload["password"]):
+        result = {"Error": "Invalid Login"}
+        if conn is not None:
+            conn.close()
+        return jsonify(result)
+
+    # TODO isto estava no meu do ano passado
+    # token = jwt.encode({'iduser': payload["iduser"],'exp': datetime.utcnow() + timedelta(minutes = 30)}, app.config['SECRET_KEY'], algorithm = "HS256")
+
+
+@app.route("/administrator/", methods=['PUT'])
+def admin_authentication():
+    # TODO fazer autenticação do admin (assumo que tenhamos de ver se o utilizador está na tabela 
+    # dos admins e depois ver se a passe bate certo na tabela dos users)
+    payload = request.get_json()
+
+
+@app.route("/artist/", methods=['PUT'])
+def admin_authentication():
+    # TODO fazer algo semelhante do admin, so que ver se o username está na tabela
+    # dos artistas e depois comparar com a pass na tabela dos users
+    payload = request.get_json()
+
+
+# ==@=== GETs ===@==
+@app.route("/user/", methods=['GET'])
+def get_all_users():
+    logger.info('GET /user')
+    payload = flask.request.get_json()
+
+    connection = db_connection()
+    cur = connection.cursor()
+
+    statement = 'SELECT * from user'
+    rows = cur.fetchall()
+
+    payload = []
+
+    for row in rows:
+        logger.debug(row)
+        content = {'username': row[0], 'password': row[1], 'full_name': row[2], 'email': row[3]}
+        payload.append(content)
+
+    response = {'status': StatusCodes['success'], 'results': payload}
+
+    conn.close()
+
+    return flask.jsonify(response)
+
+
+@app.route("/song/<keyword>", methods=['GET'])
+def search_song(keyword):
+    conn = db_connection()
+    cur = conn.cursor()
+
+    # TODO i think its fine this way
+    cur.execute("SELECT ismn, title, genre from song where ismn = %s or title = %s or genre = %s", (keyword, keyword, keyword))
+
+    rows = cur.fetchall()
+
+    payload = []
+    logging_logger.logger.debug("Songs:")
+
+    for row in rows:
+        content = {'ismn': int(row[0]), 'title': row[1], 'genre': row[2]}
+        payload.append(content)
+        logging_logger.logger.debug(row)
+
+    conn.close()
+
+    return jsonify(payload)
+
+
+@app.route("/artist/<name>", methods=['GET'])
+def detail_artist(name):
+    conn = db_connection()
+    cur = conn.cursor()
+
+    # TODO n está acabada, é preciso fazer mais cenas ig
+    cur.execute("SELECT label_name from artist where artistic_name = %s UNION select title from album where artist_userr = %s", (name, name))
+
+    rows = cur.fetchall()
+
+    payload = []
+    logging_logger.logger.debug("Artist Details:")
+
+    for row in rows:
+        content = {'Artist Name': name, 'label_name': row[0]}
+        payload.append(content)
+        logging_logger.logger.debug(row)
+
+    conn.close()
+
+    return jsonify(payload)
+
+
+
+# ==@=== FUNCTIONALITIES ===@==
+@app.route("/song/", methods=['POST'])
+@token_required
+def add_song():
+    logging_logger.logger.info('POST /song/')
+    payload = flask.request.get_json()
+
+    conn = db_connection()
+    cur = conn.cursor()
+
+    logging_logger.logger.debug(f'POST /song - payload: {payload}')
+
+    if 'ismn' not in payload:
+        response = {'status': StatusCodes['api_error'], 'results': 'ismn value not in payload'}
+        return flask.jsonify(response)
+
+    # TODO isto com ints e assim está a confundir-me um pouco, no meu do ano passado temos tudo como %s 
+    statement = 'INSERT INTO song (ismn, title, genre, duration, release_date, explicit)' \
+                'values (%d, %s, %s, %s, %s, %s)'
+    values = (int(payload['idproduct']), payload['productname'], payload['producttype'], payload['description'],
+            payload['productprice'], payload['productstock'])
+
+    try:
+        cur.execute(statement, values)
+
+        conn.commit()
+        response = {'status': StatusCodes['success'], 'results': f'Inserted song {payload["ismn"]}'}
+
+    except (Exception, psycopg2.DatabaseError) as error:
+        logging_logger.logger.error(f'POST /song - error: {error}')
+        response = {'status': StatusCodes['internal_error'], 'errors': str(error)}
+
+        conn.rollback()
+
+    finally:
+        if conn is not None:
+            conn.close()
+
+    return flask.jsonify(response)
+
+
+@app.route("/album/", methods=['POST'])
+@token_required
+def add_album():
+    # TODO kinda confused on this one
+    # é preciso inserir logo uma musica? Ou quando criamos uma musica criamos um album?
+    # E como se usa a order?
+    logging_logger.logger.info('POST /album/')
+    payload = flask.request.get_json()
 
 
 if __name__ == '__main__':
---
+++ meu_ano_passado.py
@@ -0,0 +1,292 @@
+import logging, psycopg2, time
+import flask
+import logging_logger
+from flask import jsonify
+from flask import request
+import jwt
+from datetime import datetime, timedelta
+
+app = flask.Flask(__name__)
+
+StatusCodes = {
+    'success': 200,
+    'api_error': 400,
+    'internal_error': 500
+}
+
+
+# Connect to DB
+def db_connection():
+    db = psycopg2.connect(
+        user="aulaspl",
+        password="aulaspl",
+        host="localhost",
+        port="5432",
+        database="project"
+    )
+
+    return db
+
+
+# Add user to DB
+@app.route("/dbproj/user", methods=['POST'])
+def add_user():
+    logging_logger.logger.info('POST /user')
+
+    payload = request.get_json()
+
+    conn = db_connection()
+
+    if conn is None:
+        response = {'status': StatusCodes['api_error'], 'errors': 'Connection to database failed'}
+        return jsonify(response)
+
+    cur = conn.cursor()
+
+    logging_logger.logger.info("---- NEW USER ----")
+    logging_logger.logger.debug(f'POST /user - payload: {payload}')
+
+    if 'username' not in payload:
+        response = {'status': StatusCodes['api_error'], 'results': 'username value not in payload'}
+        return flask.jsonify(response)
+
+    # parameterized queries, good for security and performance
+    statement = "INSERT INTO utilizador (username, passworduser, iduser, nameuser, isadmin) VALUES (%s, %s, %s, %s, %s)"
+    values = (payload["username"], payload["passworduser"], payload["iduser"], payload['nameuser'], payload['isadmin'])
+
+    try:
+        cur.execute(statement, values)
+        cur.execute("commit")
+        result = 'INSERTED'
+    except (Exception, psycopg2.DatabaseError) as error:
+        logging_logger.logger.error(error)
+        result = 'FAILED'
+    finally:
+        if conn is not None:
+            conn.close()
+
+    return flask.jsonify(result)
+
+
+# User autentication
+@app.route("/dbproj/user", methods = ['PUT'])
+def autenticate_user():
+    payload = request.get_json()
+
+    conn = db_connection()
+    cur = conn.cursor()
+
+    logging_logger.logger.info("AUTENTICATE /USER")
+    logging_logger.logger.debug(f'payload: {payload}')
+
+    cur.execute("SELECT passworduser FROM utilizador WHERE username=%s", (payload["username"],))
+    logging_logger.logger.debug(f'payload: {payload["username"]}')
+
+    password = cur.fetchall()
+    if (len(password) == 0):
+        result = {"Erro:": "Login Invalido"}
+        return jsonify(result)
+
+    if (password[0][0] != payload["passworduser"]):
+        result = {"Erro": "Login Invalido"}
+        if conn is not None:
+            conn.close()
+        return jsonify(result)
+    
+    token = jwt.encode({'iduser': payload["iduser"],'exp': datetime.utcnow() + timedelta(minutes = 30)}, app.config['SECRET_KEY'], algorithm = "HS256")
+
+    return jsonify({'token': token})
+
+
+# Get all users
+@app.route("/user/", methods=['GET'])
+def get_all_users():
+    logging_logger.logger.info("GET /USER")
+
+    conn = db_connection()
+    cur = conn.cursor()
+
+    cur.execute("SELECT * from utilizadores")
+    rows = cur.fetchall()
+
+    payload = []
+    logging_logger.logger.debug("---- ALL USERS ----")
+    for row in rows:
+        logging_logger.logger.debug(row)
+        content = {'username': int(row[0]), 'password': row[1], 'iduser': row[2], 'name': row[3]}
+        payload.append(content)  # appending to the payload to be returned
+
+    if conn is not None:
+        conn.close()
+
+    return flask.jsonify(payload)
+
+
+# Add new product
+@app.route("/dbproj/product", methods=['POST'])
+def add_product():
+    logging_logger.logger.info('POST /product/')
+    payload = flask.request.get_json()
+
+    conn = db_connection()
+    cur = conn.cursor()
+
+    logging_logger.logger.debug(f'POST /product - payload: {payload}')
+
+    if 'idproduct' not in payload:
+        response = {'status': StatusCodes['api_error'], 'results': 'idproduct value not in payload'}
+        return flask.jsonify(response)
+
+    statement = 'insert into produto (idproduct, productname, producttype, description, productprice, productstock)' \
+                'values (%s, %s, %s, %s, %s, %s)'
+    values = (payload['idproduct'], payload['productname'], payload['producttype'], payload['description'],
+              payload['productprice'], payload['productstock'])
+
+    try:
+        cur.execute(statement, values)
+
+        conn.commit()
+        response = {'status': StatusCodes['success'], 'results': f'Inserted produto {payload["idproduct"]}'}
+
+    except (Exception, psycopg2.DatabaseError) as error:
+        logging_logger.logger.error(f'POST /produtos - error: {error}')
+        response = {'status': StatusCodes['internal_error'], 'errors': str(error)}
+
+        conn.rollback()
+
+    finally:
+        if conn is not None:
+            conn.close()
+
+    return flask.jsonify(response)
+
+
+# Update product
+@app.route("/products/update/<id>", methods=['POST'])
+def update_product(id, description):
+    logging_logger.logger.info('POST /product/update/')
+    payload = flask.request.get_json()
+
+    conn = db_connection()
+    cur = conn.cursor()
+
+    logging_logger.logger.info('---- UPDATE PRODUCT ----')
+    logging_logger.logger.info(f'payload: {payload}')
+
+    cur = conn.cursor()
+
+    cur.execute('select productname,description,id where produtos.id=%s and produtos.description=%s', (id, description))
+
+    row = cur.fetchall()
+
+    if cur.rowcount == 0:
+        conn.close()
+        logging_logger.logger.error(f'POST /produtos - error: {logging.error}')
+        response = {'status': StatusCodes['internal_error'], 'errors': str(logging.error)}
+
+    statement = "update produtos set productprice=%s,productstock=%s,"
+
+
+#Generic product information
+@app.route("/dbproj/product/<product_id>", methods=['GET'])
+def productInfo(id):
+    conn = db_connection()
+    cur = conn.cursor()
+
+    cur.execute("SELECT idproduct, description from produtos where idproduct = %s and produtos.description = %s", (id,))
+
+    rows = cur.fetchall()
+    cur.execute("SELECT texto,data_user_username from mensagem_mural where leiloes_id_leilao=%s", (id,))
+
+    payload = []
+    logging_logger.logger.debug("Products:")
+
+    for row in rows:
+         content = {'idproduct': int(row[0]), 'description': row[1]}
+         payload.append(content)
+         logging_logger.logger.debug(row)
+
+    conn.close()
+
+    return jsonify(payload)
+
+
+#Purchase product____________________________________Terminar_____________________________
+@app.route('/dbproj/order', methods = ['PUT'])
+def purchase():
+    token = request.headers.get('authtoken')
+
+    if token is None:
+        response = {'status': StatusCodes['req_error'], 'errors': 'Authentication token missing'}
+    else:    
+        userinfo = jwt.decode(token, "SECRET_KEY", algorithms = "HS256")
+
+    payload = request.get_json()
+    required = {'encomenda'}
+
+    fields = set(payload.keys())
+    diff = list(required.difference(fields))
+
+    if (len(diff) > 0):
+        response = {'status': StatusCodes['req_error'], 'errors': f'Missing fields {diff}'}
+        return jsonify(response)
+
+    encomenda = payload["encomenda"]
+
+    conn = db_connection()
+
+    try:
+        conn.cursor().execute("BEGIN")
+        conn.cursor().execute("SELECT FROM comprador WHERE iduser = %s", (userinfo['iduser'],))
+
+        if (conn.cursor().fetchone() is None):
+            response = {'status': StatusCodes['req_error'], 'errors': 'User registered is not a buyer'}
+            return jsonify(response)
+        else:
+            conn.cursor().execute("SELECT balance FROM user WHERE iduser = %s", (userinfo['iduser'],))
+            balance = conn.cursor().fetchone()
+            idPurchase = idGenerator()
+
+    except psycopg2.Error as e:
+        response = {'status': StatusCodes['api_error'], 'errors': str(e)}
+
+    return jsonify(response)
+
+
+#Functions
+def idGenerator(colum, table):
+    conn = db_connection()
+    cursor = conn.cursor()
+
+    cursor.execute(f"SELECT COUNT({colum}) FROM {table}")
+    result = cursor.fetchone()
+
+    if result[0] is None:
+        return 0
+
+    return result[0] + 1
+
+
+def main():
+    # Set up the logging
+    logging.basicConfig(filename="log_file.log")
+    logging_logger.logger = logging.getLogger('logger')
+    logging_logger.logger.setLevel(logging.DEBUG)
+    ch = logging.StreamHandler()
+    ch.setLevel(logging.DEBUG)
+    # create formatter
+    formatter = logging.Formatter('%(asctime)s [%(levelname)s]:  %(message)s', '%H:%M:%S')
+    # "%Y-%m-%d %H:%M:%S") # not using DATE to simplify
+    ch.setFormatter(formatter)
+    logging_logger.logger.addHandler(ch)
+
+    time.sleep(1)  # just to let the DB start before this print :-)
+
+    logging_logger.logger.info("\n---------------------------------------------------------------\n" +
+                        "API v1.0 online: http://localhost:8080\n\n")
+
+    app.run(host="0.0.0.0", debug=True, threaded=True, port=8080)
+
+
+if __name__ == "__main__":
+    main()
